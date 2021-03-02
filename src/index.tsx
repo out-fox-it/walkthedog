c93ad3c23eb62433ddca6ab6ce4f808f1c59c79b
agnes97 <janachaloupkova97@gmail.com>
2021-03-02 13:24:52 +0100

    Add Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { CssBaseline, ThemeProvider } from '@material-ui/core'
+import firebase from 'firebase'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from '../src/components/App/App'
+import firebaseConfig from './config/firebaseConfig'
 import './index.css'
 import theme from './theme'
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 ReactDOM.render(
 	<React.StrictMode>
